262318a3ba79e4397ebe66d4a152c9a2b225da01
Surabhi Gaopande <surabhi.gaopande@gmail.com>
2017-10-25 15:21:47 -0700

    Updated main file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# This app serves quotes from famous women in STEM by author and topic, and
-# serves bios by author.
-# Created for the "Building a Conversational Agent" workshop at the 2017
-# Grace Hopper Conference.
+# This app gives different features for the various Wi-Fi technologies.
+
 
 import csv
 import logging
@@ -14,118 +12,109 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-# List of (quote, author, (topic1, topic2, ...)) tuples.
-quotes = []
+# List of (wifi tech, feature, urls).
+wifi = []
 
-# Map from author string to list of quote objects.
-quotes_by_author = {}
+# Map from technology string to list of wifi objects.
+wifi_by_tech = {}
 
-# Map from topic string to list of quote objects.
-quotes_by_topic = {}
+# Map from url string to list of wifi objects.
+wifi_by_url = {}
 
-# Map from author string to bio string.
-bio_by_author = {}
 
-# Read in quotes and bios.
-with open('quotes.csv', 'rb') as quotes_file:
-  quotes_reader = csv.reader(quotes_file)
+
+# Read in wifi tech and features.
+with open('protocols.csv', 'rb') as wifi_file:
+  feature_reader = csv.reader(wifi_file)
 
   # Skip the header line.
-  next(quotes_reader, None)
+  next(feature_reader, None)
 
   # Each line of the csv has the format
-  # "quote, author, year_born, year_died, bio, date_spoken, source, topics,
-  # comments".
-  for row in quotes_reader:
-    quote = row[0]
-    author = row[1]
-    bio = row[4]
-    topics = tuple(x.lower().strip() for x in row[7].split(','))
-    quote_object = (quote, author, topics)
-    quotes.append(quote_object)
-    normalized_author = author.lower()
-    if normalized_author not in quotes_by_author:
-      quotes_by_author[normalized_author] = []
-    quotes_by_author[normalized_author].append(quote_object)
-    for topic in list(topics):
-      if topic not in quotes_by_topic:
-        quotes_by_topic[topic] = []
-      quotes_by_topic[topic].append(quote_object)
-    if normalized_author not in bio_by_author:
-      bio_by_author[normalized_author] = bio
-
+  # "technology, feature, url".
+  for row in feature_reader:
+    feature = row[0]
+    tech = row[1]
+    url= row[2]
+    wifi_object = (feature, tech, url)
+    wifi.append(wifi_object)
+    normalized_tech = tech.lower()
+    if normalized_tech not in wifi_by_tech:
+      wifi_by_tech[normalized_tech] = []
+    wifi_by_tech[normalized_tech].append(wifi_object)
+   
 # Exception for a bad request from the client.
 class BadRequestError(ValueError):
   pass
 
-# Returns a quote object matching the parameters of the request, or None if
-# there are no matching quotes.
-def _get_quote():
-  # Extract the author and topic parameters. For robustness, the parameter
+# Returns a wifi feature object matching the parameters of the request, or None if
+# there are no matching features.
+def _get_feature():
+  # Extract the tech and url parameters. For robustness, the parameter
   # names can be capitalized in any way.
-  author = None
-  topic = None
+  tech = None
+  url = None
   parameters = request.json['result']['parameters']
   if parameters:
     for key, value in parameters.items():
-      if key.lower() == 'author':
+      if key.lower() == 'tech':
         author = unicodedata.normalize('NFKC', value).lower()
-      elif key.lower() == 'topic':
+      elif key.lower() == 'url':
         topic = unicodedata.normalize('NFKC', value).lower()
       else:
         raise BadRequestError('Unrecognized parameter in request: ' + key)
 
-  # Find the set of quotes by the given author (all quotes if not specified).
-  applicable_author_quotes = set()
-  if author:
-    if author in quotes_by_author:
-      applicable_author_quotes = set(quotes_by_author[author])
+  # Find the set of features for a given tech (all features if not specified).
+  applicable_tech_features = set()
+  if tech:
+    if tech in wifi_by_tech:
+      applicable_tech_features = set(wifi_by_tech[tech])
   else:
-    applicable_author_quotes = set(quotes)
+    applicable_tech_features = set(feature)
 
-  # Find the set of quotes on the given topic (all quotes if not given).
-  applicable_topic_quotes = set()
-  if topic:
-    if topic in quotes_by_topic:
-      applicable_topic_quotes = set(quotes_by_topic[topic])
-  else:
-    applicable_topic_quotes = set(quotes)
+  # # Find the set of quotes on the given topic (all quotes if not given).
+  # applicable_topic_quotes = set()
+  # if topic:
+    # if topic in quotes_by_topic:
+      # applicable_topic_quotes = set(quotes_by_topic[topic])
+  # else:
+    # applicable_topic_quotes = set(quotes)
 
-  # The matching quotes are in the intersection of the two sets.
-  applicable_quotes = applicable_author_quotes.intersection(applicable_topic_quotes)
+  # # The matching quotes are in the intersection of the two sets.
+  # applicable_quotes = applicable_author_quotes.intersection(applicable_topic_quotes)
 
-  # Return None if there are no matching quotes.
-  if len(applicable_quotes) == 0:
-    return None
+  # # Return None if there are no matching quotes.
+  # if len(applicable_quotes) == 0:
+    # return None
 
   # Return one of the matching quotes randomly.
-  quote_to_return = random.choice(tuple(applicable_quotes))
+  feature_to_return = random.choice(applicable_tech_features)
 
-  return quote_to_return
+  return feature_to_return
 
-# Returns the bio of the author specified in the parameters as a string, or
-# None if there is no matching author.
-def _get_bio():
-  # Extract the author parameter. For robustness, the parameter name can be
-  # capitalized in any way.
-  parameters = request.json['result']['parameters']
-  author = None
-  for key, value in parameters.items():
-    if key.lower() == 'author':
-      author = unicodedata.normalize('NFKC', value).lower()
-    else:
-      raise BadRequestError('Unrecognized parameter in request: ' + key)
+# # Returns the bio of the author specified in the parameters as a string, or
+# # None if there is no matching author.
+# def _get_bio():
+  # # Extract the author parameter. For robustness, the parameter name can be
+  # # capitalized in any way.
+  # parameters = request.json['result']['parameters']
+  # author = None
+  # for key, value in parameters.items():
+    # if key.lower() == 'author':
+      # author = unicodedata.normalize('NFKC', value).lower()
+    # else:
+      # raise BadRequestError('Unrecognized parameter in request: ' + key)
 
-  if not author:
-    raise BadRequestError('No author parameter provided in request for bio.')
+  # if not author:
+    # raise BadRequestError('No author parameter provided in request for bio.')
 
-  # Return the bio if we have it, None otherwise.
-  if author:
-    return bio_by_author.get(author)  # returns None if key does not exist
-  else:
-    return None
+  # # Return the bio if we have it, None otherwise.
+  # if author:
+    # return bio_by_author.get(author)  # returns None if key does not exist
+  # else:
+    # return None
 
-class QuoteSearch(Resource):
+class FeatureSearch(Resource):
   # Handles a request from API.AI. The relevant part of the body is:
   # {
   #   "result": {
@@ -151,56 +140,56 @@ class QuoteSearch(Resource):
 
       action = request.json['result']['action']
 
-      if action == 'get_quote_event':
-        quote = _get_quote()
-        if quote:
+      if action == 'get_feature_event':
+        feature = _get_feature()
+        if feature:
           response_body = {
             'followupEvent': {
-                'name': 'respond_with_quote',
+                'name': 'respond_with_feature',
                 'data': {
-                    'quote': quote[0],
-                    'author': quote[1]
+                    'feature': feature[0],
+                    'tech': feature[1]
                 }
             }}
         else:
           response_body = {
             'followupEvent': {
-                'name': 'respond_with_quote',
+                'name': 'respond_with_feature',
                 'data': {}
             }}
 
-      elif action == 'get_quote_response':
-        quote = _get_quote()
+      elif action == 'get_feature_response':
+        feature = _get_feature()
         if quote:
-          response = 'Here is a quote by ' + quote[1] + ': ' + quote[0]
+          response = 'Here is an interesting WiFi feature for the ' + feature[1] + ' technology : ' + feature[1]
         else:
-          response = 'I have no matching quote.'
+          response = 'I have no matching WiFi feature.'
         response_body = {'speech': response, 'displayText': response}
 
-      elif action == 'get_bio_event':
-        bio = _get_bio()
-        if bio:
-          response_body = {
-            'followupEvent': {
-                'name': 'respond_with_bio',
-                'data': {
-                    'bio': bio
-                }
-            }}
-        else:
-          response_body = {
-            'followupEvent': {
-                'name': 'respond_with_bio',
-                'data': {}
-            }}
+      # elif action == 'get_bio_event':
+        # bio = _get_bio()
+        # if bio:
+          # response_body = {
+            # 'followupEvent': {
+                # 'name': 'respond_with_bio',
+                # 'data': {
+                    # 'bio': bio
+                # }
+            # }}
+        # else:
+          # response_body = {
+            # 'followupEvent': {
+                # 'name': 'respond_with_bio',
+                # 'data': {}
+            # }}
 
-      elif action == 'get_bio_response':
-        bio = _get_bio()
-        if bio:
-          response = 'Here is the bio: ' + bio
-        else:
-          response = 'I have no matching bio.'
-        response_body = {'speech': response, 'displayText': response}
+      # elif action == 'get_bio_response':
+        # bio = _get_bio()
+        # if bio:
+          # response = 'Here is the bio: ' + bio
+        # else:
+          # response = 'I have no matching bio.'
+        # response_body = {'speech': response, 'displayText': response}
 
       else:
         raise BadRequestError('Request action unrecognized: "' + action + '"')
@@ -212,8 +201,8 @@ class QuoteSearch(Resource):
       response.status_code = 400
       return response
 
-# Register the quotesearch endpoint to be handled by the QuoteSerch class.
-api.add_resource(QuoteSearch, '/quotesearch')
+# Register the wifisearch endpoint to be handled by the FeatureSearch class.
+api.add_resource(FeatureSearch, '/wifisearch')
 
 if __name__ == '__main__':
   app.run()
